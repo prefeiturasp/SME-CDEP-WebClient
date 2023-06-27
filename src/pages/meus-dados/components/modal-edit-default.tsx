03cfc057f8d23d35498a2ecc254a4f0f97fc2b07
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2023-06-27 14:15:17 -0300

    Meus dados - add componente confirm

--- src/pages/meus-dados/components/modal-edit-default.tsx
+++ src/pages/meus-dados/components/modal-edit-default.tsx
@@ -4,6 +4,10 @@ import { AxiosResponse } from 'axios';
 import React, { PropsWithChildren, useState } from 'react';
 import Modal from '~/components/lib/modal';
 import { DadosUsuarioDTO } from '~/core/dto/dados-usuario-dto';
+import { Modal as ModalAntd } from 'antd';
+import { Colors } from '~/core/styles/colors';
+
+const { confirm } = ModalAntd;
 
 type ModalEditDefaultProps = {
   service: (values: DadosUsuarioDTO) => Promise<AxiosResponse<boolean>>;
@@ -39,9 +43,36 @@ const ModalEditDefault: React.FC<ModalEditDefaultProps> = ({
       .finally(() => setLoading(false));
   };
 
+  const validateFields = () => {
+    form.validateFields().then(() => {
+      handleOk();
+    });
+  };
+
   const handleCancel = () => {
     form.resetFields();
     setOpen(false);
+  };
+
+  const showConfirmCancel = () => {
+    if (form.isFieldsTouched()) {
+      // TODO - Componente confirm vai ser criado na próxima sprint!
+      confirm({
+        title: 'Atenção',
+        icon: <></>,
+        content: 'Você não salvou, deseja descartar a alteração?',
+        onOk() {
+          handleCancel();
+        },
+        okButtonProps: { type: 'default' },
+        cancelButtonProps: {
+          type: 'text',
+          style: { color: Colors.TEXT },
+        },
+      });
+    } else {
+      handleCancel();
+    }
   };
 
   return (
@@ -50,8 +81,8 @@ const ModalEditDefault: React.FC<ModalEditDefaultProps> = ({
       <Modal
         open={open}
         title={title}
-        onOk={handleOk}
-        onCancel={handleCancel}
+        onOk={validateFields}
+        onCancel={showConfirmCancel}
         centered
         destroyOnClose
         cancelButtonProps={{ disabled: loading }}
