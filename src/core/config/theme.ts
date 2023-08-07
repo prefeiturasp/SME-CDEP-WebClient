cccf553a4908509ffa03bfaca6fad7a06a733679
Neder <nedersimoes@gmail.com>
2023-08-07 16:35:17 -0400

    alteracao de logo e tema

--- src/core/config/theme.ts
+++ src/core/config/theme.ts
@@ -5,15 +5,15 @@ export const CDEPTheme: ThemeConfig = {
   token: {
     fontFamily: 'Roboto',
     colorText: Colors.TEXT,
-    colorPrimary: Colors.BLUE_CDEP,
+    colorPrimary: Colors.CDEP_PRIMARY,
     borderRadius: 4,
     controlHeight: 38,
     colorError: Colors.ERROR,
   },
   components: {
     Button: {
-      colorText: Colors.BLUE_CDEP,
-      colorBorder: Colors.BLUE_CDEP,
+      colorText: Colors.CDEP_PRIMARY,
+      colorBorder: Colors.CDEP_PRIMARY,
     },
     Layout: {
       colorBgLayout: Colors.BACKGROUND_CONTENT,
