824b3ccc59b57747b43da0bd689518e67fdc041e
Neder <nedersimoes@gmail.com>
2023-08-11 16:05:21 -0400

    componente de auditoria

--- src/core/utils/functions/index.ts
+++ src/core/utils/functions/index.ts
@@ -1,1 +1,9 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 export const removerTudoQueNaoEhDigito = (value: any) => `${value}`.replace(/\D/g, '');
+
+export const formatarDataHora = (data: string) =>
+  format(new Date(data), "dd/MM/yyyy 'às' HH:mm", {
+    locale: ptBR,
+  });
