261e5cb8a6089ff5a7fa788bf13db16715f97b9a
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2024-01-24 17:41:08 -0300

    Solicitação - download de arquivos

--- src/pages/operacoes/solicitacao/components/btn-enviar-solicitacao/index.tsx
+++ src/pages/operacoes/solicitacao/components/btn-enviar-solicitacao/index.tsx
@@ -87,8 +87,8 @@ const BtnEnviarSolicitacoes: React.FC = () => {
   };
 
   useEffect(() => {
-    obterTermo();
-  }, []);
+    if (showModal) obterTermo();
+  }, [showModal]);
 
   const handleCheckboxChange = (e: CheckboxChangeEvent) => {
     setChecked(e.target.checked);
