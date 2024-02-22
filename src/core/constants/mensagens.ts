c46d32f6d72e06edc323c56bca36ce7ca521d241
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-22 09:43:05 -0300

    Correção da mensagem

--- src/core/constants/mensagens.ts
+++ src/core/constants/mensagens.ts
@@ -4,7 +4,7 @@ export const ERRO_INFORMAR_USUARIO_SENHA =
 export const ERRO_LOGIN = 'Erro ao tentar autenticar no servidor';
 export const ERRO_CADASTRO_USUARIO = 'Erro ao tentar cadastrar usuário';
 export const DESEJA_CANCELAR_ALTERACOES =
-  'Você não salvou as informações preenchidas, Deseja realmente cancelar as alterações?';
+  'Você não salvou as informações preenchidas, deseja realmente cancelar as alterações?';
 export const ERRO_INFORMAR_LOGIN_RECUPERACAO_SENHA =
   'Você precisa um login para recuperação de senha';
 export const ERRO_RECUPERACAO_SENHA = 'Erro ao tentar recuperar senha';
