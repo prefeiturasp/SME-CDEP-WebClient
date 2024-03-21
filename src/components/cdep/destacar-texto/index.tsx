f11dcddd4acc417b2f0b3cd3f69adb7a1d502afd
Neder Hayden <nedersimoes@gmail.com>
2024-03-21 10:57:19 -0400

    Critério 2 - Destacar a palavra pesquisada

---
+++ src/components/cdep/destacar-texto/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface DestacarTextoProps {
+  palavraPraDestacar: string;
+  palavraComparacao?: string;
+}
+
+export const DestacarTexto: React.FC<DestacarTextoProps> = ({
+  palavraPraDestacar,
+  palavraComparacao,
+}) => {
+  const removerAcentos = (str: string): string => {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
+  if (!palavraComparacao) return palavraPraDestacar;
+
+  const regex = new RegExp(`(${palavraComparacao.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`, 'gi');
+
+  return palavraPraDestacar.split(' ').map((word, index, words) => {
+    const normalizarPalavra = removerAcentos(word).toLowerCase();
+    const destacada = regex.test(normalizarPalavra);
+
+    return (
+      <React.Fragment key={index}>
+        <span style={{ backgroundColor: destacada ? 'yellow' : 'transparent' }}>{word}</span>
+        {index < words.length - 1 ? ' ' : ''}
+      </React.Fragment>
+    );
+  });
+};
