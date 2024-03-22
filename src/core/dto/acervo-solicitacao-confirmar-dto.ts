9e8296081c1ab35aabf0fb07d0ff8e2c41092af9
Neder Hayden <nedersimoes@gmail.com>
2024-03-22 16:33:12 -0400

    Critério 4 - Abrir tela auxiliar com os campos editáveis

--- src/core/dto/acervo-solicitacao-confirmar-dto.ts
+++ src/core/dto/acervo-solicitacao-confirmar-dto.ts
@@ -1,6 +1,12 @@
-import { AcervoSolicitacaoItemConfirmarDTO } from './acervo-solicitacao-item-confirmar-dto';
+import { Dayjs } from '../date/dayjs';
+import { TipoAtendimentoEnum } from '../enum/tipo-atendimento-enum';
 
 export type AcervoSolicitacaoConfirmarDTO = {
   id: number;
-  itens: AcervoSolicitacaoItemConfirmarDTO[];
+  itemId?: number;
+  tipoAtendimento?: TipoAtendimentoEnum;
+  dataVisita?: Dayjs;
+  dataEmprestimo?: string;
+  dataDevolucao?: string;
+  tipoAcervo?: number;
 };
