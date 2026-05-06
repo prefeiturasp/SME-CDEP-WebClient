7e9c5bf6d2f97338c8bfab823801487f3f530c9d
Alex Paulo <alexpaulo.pc@gmail.com>
2026-05-06 10:05:42 -0300

    Criação dos testes da tela Crédito.

--- testes/cypress/support/e2e.js
+++ testes/cypress/support/e2e.js
@@ -11,6 +11,7 @@ require('./commands_ui/commands_solicitacoes')
 require('./commands_ui/commands_meus_dados')
 require('./commands_ui/commands_dashboards')
 require('./commands_ui/commands_assunto') 
+require('./commands_ui/commands_credito')
 
 // Evita falhas silenciosas caso algum comando seja removido ou renomeado
 Cypress.on('uncaught:exception', (err, runnable) => {
